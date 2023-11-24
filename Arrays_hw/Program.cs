b158099a168d01f3788c368ad10e5c46abefd9e6
oa-kalinina@yandex.ru <oa-kalinina@yandex.ru>
2023-11-24 21:41:29 +0300

    commit

--- Arrays_hw/Program.cs
+++ Arrays_hw/Program.cs
@@ -1,6 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 // Домашняя работа
+// Задача 1
+
+Console.WriteLine("Введите любое число :");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = 0;
 int[] array = { 1, 9, 3, 6, 5 };
-Console.WriteLine(array);
-//    console.
+foreach (int i in array)
+{
+    if (i == a)
+    {
+       Console.WriteLine($"Веденное число входит в массив");
+        b = 1;
+    }
+ }
+if (b == 0)
+{
+    Console.WriteLine($"Веденное число не входит в массив");
+}
+
