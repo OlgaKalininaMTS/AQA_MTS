e4046dcb383ea05993d5f911c76792a7c86f2fbe
oa-kalinina@yandex.ru <oa-kalinina@yandex.ru>
2023-11-25 11:23:32 +0300

    commit hw

--- Arrays_hw/Program.cs
+++ Arrays_hw/Program.cs
@@ -46,13 +46,72 @@ else
     Console.WriteLine(str);
 }
 */
-
 // Задача 3
-int[] spi1 = { 1, 2, 3, 4, 5 };
+
+Console.WriteLine("Введите размер массива (число от 0 до 100):");
+int a = Convert.ToInt16(Console.ReadLine());
+
+int[] array = new int[a];
+Random rand = new Random();
+for (int i = 0; i < array.Length; i++)
+    array[i] = rand.Next(100);
+
+Console.WriteLine();
+foreach (int item in array)
+{
+    Console.Write($"{item} ");
+}
+
+int max = array.Max();
+Console.Write($"\n Максимальное значение массива: {max}");
+
+int min = array.Min();
+Console.Write($"\n Минимальное значение массива: {min}");
+
+int summ = 0;
+for (int i = 0; i < array.Length; i++)
+    summ += array[i];
+int aver = summ / array.Length;
+Console.WriteLine($"\n Среднее значение массива: {aver}");
+
+// Задача 4
+/*
+int[] spi1 = { 5, 4, 3, 2, 1 };  //3
 var str1 = string.Join(" ", spi1);
-Console.WriteLine(str1);
-int[] spi2 = { 6, 7, 8, 9, 10 };
+
+int[] spi2 = { 5, 4, 100, 2, 1 };  //8
 var str2 = string.Join(" ", spi2);
-Console.WriteLine(str2);
+
+Console.WriteLine($"Массив 1:{str1}\nМассив 2:{str2}");
+
+// среднее арифметическое массива 1
+int summ = 0;
+for (int i = 0; i < spi1.Length; i++)
+    summ += spi1[i];
+int mid = summ / spi1.Length;
+Console.WriteLine($"Среднее арифметическое массива 1: {mid}");
+
+// среднее арифметическое массива 2
+int summ2 = 0;
+for (int i = 0; i < spi2.Length; i++)
+    summ2 += spi2[i];
+int mid2 = summ2 / spi1.Length;
+Console.WriteLine($"Среднее арифметическое массива 2: {mid2}");
+
+if (mid > mid2)
+{
+     Console.WriteLine("Среднее арифметическое массива 1 больше среднего арифметического массива 2");
+}
+if (mid < mid2)
+{
+     Console.WriteLine("Среднее арифметическое массива 1 меньше среднего арифметического массива 2");
+}
+if (mid == mid2)
+{
+     Console.WriteLine("Средние арифметические массива 1 и массива 2 равны");
+}
+*/
+
+
 
 
