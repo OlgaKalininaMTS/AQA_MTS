d788b278ff888679cc7cdb6fc683b991ef8ec81e
oa-kalinina@yandex.ru <oa-kalinina@yandex.ru>
2023-12-02 12:36:40 +0500

    commit

--- Classes/Phone.cs
+++ Classes/Phone.cs
@@ -1,18 +1,40 @@
-﻿namespace Classes.Models;
+﻿using System.Xml.Linq;
+
+namespace Classes;
 
 public class Phone
 {
     // Блок полей (переменных)
     public int number;   // номер
-    public int model;      // возраст
+    public int model;      // модель
     public int weight;    // вес
-    
+    public string name;
 
-    // Блок конструктора
-
-    // Блок методов и свойств
     public void Print()
     {
-        Console.WriteLine($"Имя: {number}  Возраст: {model} Возраст: {weight}");
+        Console.WriteLine($"Номер: {number}  Модель: {model} Вес: {weight}");
+    }
+    public void receiveCall(string name)
+    {
+        Console.WriteLine($"Звонит: {name}");
+    }
+    public void getNumber(int number)
+    {
+        Console.WriteLine($"Номер телефона: {number}");
     }
 }
+
+public class ConstructorPhone
+{
+    public ConstructorPhone()
+    {
+    }
+    public ConstructorPhone(int number, int model) // третий конструктор
+    {
+    }
+    public ConstructorPhone(int number, int model, int weight) // третий конструктор
+    {
+    }
+    
+}
+
