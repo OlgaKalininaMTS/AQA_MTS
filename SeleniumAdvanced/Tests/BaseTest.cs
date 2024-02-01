5f418dbb8fcd6022d32b5f1e904e4c44f671d43d
oa-kalinina@yandex.ru <oa-kalinina@yandex.ru>
2024-02-01 20:13:42 +0500

    commit

---
+++ SeleniumAdvanced/Tests/BaseTest.cs
@@ -0,0 +1,27 @@
+using NUnitTest.Core;
+using NUnitTest.Helpers;
+using NUnitTest.Helpers.Configuration;
+using OpenQA.Selenium;
+
+namespace NUnitTest.Tests;
+
+[Parallelizable(scope: ParallelScope.All)]
+[FixtureLifeCycle(LifeCycle.InstancePerTestCase)]
+public class BaseTest
+{
+    protected IWebDriver Driver { get; private set; }
+    protected WaitsHelper WaitsHelper { get; private set; }
+
+    [SetUp]
+    public void Setup()
+    {
+        Driver = new Browser().Driver;
+        WaitsHelper = new WaitsHelper(Driver, TimeSpan.FromSeconds(Configurator.WaitsTimeout));
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        Driver.Quit();
+    }
+}
