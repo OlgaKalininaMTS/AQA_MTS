bfea69e1515c4a736554ca46334b0bb6e52e44df
oa-kalinina@yandex.ru <oa-kalinina@yandex.ru>
2024-02-03 13:45:24 +0500

    commit

--- NUnitProject/Calc.cs
+++ NUnitProject/Calc.cs
@@ -2,5 +2,20 @@
 
 public static class Calc
 {
+    public static int Div(int a, int b)
+{
+    return a / b;
+}
+
+public static double Div(double a, double b)
+{
+    return a / b;
+}
+
+}
+
+/*public static class Calc
+{
     public static int Sum(int x, int y) => x + y;
 }
+*/
