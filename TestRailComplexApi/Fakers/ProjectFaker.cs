32163eb2338445c3fbaf29131b5a0f997bbcf26f
oa-kalinina@yandex.ru <oa-kalinina@yandex.ru>
2024-03-28 20:21:16 +0500

    commit

---
+++ TestRailComplexApi/Fakers/ProjectFaker.cs
@@ -0,0 +1,15 @@
+﻿using Bogus;
+using TestRailComplexApi.ApiTesting;
+
+namespace TestRailComplexApi.Fakers;
+
+public sealed class ProjectFaker : Faker<Project>
+{
+    public ProjectFaker()
+    {
+        RuleFor(b => b.Name, f => f.Random.AlphaNumeric(20)); // 
+        RuleFor(b => b.Announcement, f => f.Random.Words(50));
+        RuleFor(b => b.SuiteMode, f => f.Random.Number(1, 3));
+        RuleFor(b => b.ShowAnnouncement, f => f.Random.Bool());
+    }
+}
